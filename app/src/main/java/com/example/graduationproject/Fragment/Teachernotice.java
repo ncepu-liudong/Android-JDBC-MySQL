9b3e56cb4433e38799b14cf4c388fe7767fad9e5
ncepu-liudong <46965739+ncepu-liudong@users.noreply.github.com>
2019-05-15 15:06:32 +0800

    5.15 15:06

--- app/src/main/java/com/example/graduationproject/Fragment/Teachernotice.java
+++ app/src/main/java/com/example/graduationproject/Fragment/Teachernotice.java
@@ -8,10 +8,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.graduationproject.R;
 
 public class Teachernotice extends Fragment {
+    private TextView notice;
+    private Button new_notice;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -22,7 +26,16 @@ public class Teachernotice extends Fragment {
         init(view);
     }
     private void init(View view){
+        notice = view.findViewById(R.id.notice);
 
+
+        new_notice = view.findViewById(R.id.new_notice);
+        new_notice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/graduationproject/TeacherNewNotice.java
@@ -0,0 +1,13 @@
+package com.example.graduationproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherNewNotice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_new_notice);
+    }
+}
